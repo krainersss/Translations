0f1470fbad3e76033f3dab29e8ab3d10c50de5f7
allie <159778986+krainersss@users.noreply.github.com>
2024-11-13 15:11:51 +0800

    Add files via upload

--- public/signup.js
+++ public/signup.js
@@ -2,32 +2,33 @@ let pw_confirm = document.getElementById("pw_confirm")
 let error = document.getElementById("error")
 let form = document.getElementById("form")
 
-// function authenticate() {
-//     error.style.display="none"
-//     if (pw_input.value === ""){
-//         error.style.display="block"
-//         error.textContent="Please enter your password"
-//     }
-//     if (pw_input.value.length < 8) { 
-//         error.style.display="block"
-//         error.textContent="Password must be at least 8 characters"
-//     }
-//     if (!(/[A-Z]/.test(pw_input.value))) {
-//         error.style.display="block"
-//         error.textContent="Password must contain a capital letter."
-//     }
-//     if (!(/[0-9]/.test(pw_input.value))) { 
-//         error.style.display="block"
-//         error.textContent="Password must contain a number"
-//     }
-//     if (!(pw_input.value === pw_confirm.value)) {
-//         error.style.display="block"
-//         error.textContent="New Password and Password Confirm DO NOT MATCH"
-//     }    
-// }
+function authenticate() {
+    error.style.display="none"
+    if (pw_input.value === ""){
+        error.style.display="block"
+        error.textContent="Please enter your password"
+    }
+    if (pw_input.value.length < 8) { 
+        error.style.display="block"
+        error.textContent="Password must be at least 8 characters"
+    }
+    if (!(/[A-Z]/.test(pw_input.value))) {
+        error.style.display="block"
+        error.textContent="Password must contain a capital letter."
+    }
+    if (!(/[0-9]/.test(pw_input.value))) { 
+        error.style.display="block"
+        error.textContent="Password must contain a number"
+    }
+    if (!(pw_input.value === pw_confirm.value)) {
+        error.style.display="block"
+        error.textContent="New Password and Password Confirm DO NOT MATCH"
+    }    
+}
 
 document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    var name = document.getElementById('fname').value
     var email = document.getElementById('email_inp').value
     var a_password = document.getElementById("pw_inp").value
     var confirmPassword = pw_confirm.value
@@ -36,10 +37,14 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
         alert('Passwords do not match. Please try again.');
         return;
     }
+    else if (a_password.length <= 8) {
+        alert('Password should be atleast more than 8 characters. Please try again.');
+        return;
+    }
     fetch("/signup", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({inp_email: email, inp_password: a_password})
+        body: JSON.stringify({inp_email: email, inp_password: a_password, name: name})
     })
     .then(response => response.json())
     .then(data => {
