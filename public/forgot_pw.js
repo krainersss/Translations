33ec90b1cef4e323e9fc9dee00cb3b5472130cf2
allie <159778986+krainersss@users.noreply.github.com>
2024-11-13 12:56:46 +0800

    Add files via upload

--- public/forgot_pw.js
+++ public/forgot_pw.js
@@ -1,19 +1,25 @@
-let form = document.getElementById("form").addEventListener(submit, (event) => {
+let form = document.getElementById("form").addEventListener("submit", (event) => {
     event.preventDefault();
     var input_email = document.getElementById("name").value
-    fetch('/forgot-password', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({email: input_email})
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert("Email sent Successfully!")
-            window.location.href = "reset-password.html"
+    fetch("/forgot-password", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email: input_email}),
+        credentials: 'include'
+    }).then(response => {
+        return response.json().then(data => {
+            if (!response.ok) {
+                throw new Error("BOOMBAYA")
+            }
+            console.log(data)
+            return data;
+        })
+    }).then(data => {
+        if (!data.success) {
+            alert(data.message)
         } else {
-            alert("An error occured")
+            alert(data.message)
+            window.location.href = "reset-password.html"
         }
     })
-    .catch(error => console.error("error", error))
 })
